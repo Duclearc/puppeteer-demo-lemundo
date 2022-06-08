20cf082edffb9da0adbd1aba8cab233598bcea7d
Dan Pina <dan@Duclearc-MBP.fritz.box>
2022-06-08 16:18:57 +0200

    [master] step 2 complete

--- puppeteer.js
+++ puppeteer.js
@@ -1,1 +1,29 @@
-console.log('puppeteer demo activated')
+const ppt = require('puppeteer');
+
+const viewports = [
+    { width: 390, height: 844 },  // iPhone 12
+    { width: 820, height: 1180 }, // iPad Air
+    { width: 1536, height: 960 }  // MacBook Pro 16"
+];
+
+(async () => {
+    // oppening browser
+    const browser = await ppt.launch({
+        headless: false
+    });
+
+    const page = await browser.newPage();
+
+    // going through viewports
+    for (let i = 0; i < viewports.length; i++) {
+        const vp = viewports[i];
+        console.log({vp});
+        await page.setViewport(vp);
+        await page.goto('https://github.com/Duclearc');
+    }
+
+    setTimeout(async () => {
+        // closing browser
+        await browser.close()
+    }, 3000);
+})()
